708bb4af71fa725933482a64bf023601be8f2836
webitproff <webitproff@gmail.com>
2026-06-23 15:52:00 +0300

    Update visitor_stats.setup.php

--- visitor_stats/visitor_stats.setup.php
+++ visitor_stats/visitor_stats.setup.php
@@ -5,11 +5,11 @@ Code=visitor_stats
 Name=Visitor Statistics with Crawler Detection
 Category=stats
 Description=Advanced visitor statistics with crawler/bot detection
-Version=1.0.0
-Date=2026-06-22
+Version=1.0.27
+Date=June 23Th, 2026
 Author=webitproff
 Copyright=(c) webitproff 2026 https://github.com/webitproff/visitor-stats-crawler-cotonti
-Notes=no composer for install
+Notes=
 Auth_guests=R
 Lock_guests=W12345A
 Auth_members=R
@@ -19,6 +19,20 @@ Requires_plugins=
 ==================== */
 
 defined('COT_CODE') or die('Wrong URL');
-// BSD-3-Clause license
-// https://github.com/webitproff/visitor-stats-crawler-cotonti
-// /plugins/visitor_stats/visitor_stats.setup.php
+
+/**
+ * visitor_stats.setup.php - Register data in $db_core and $db_config. Setup & Config File for the Plugin visitor_stats
+ *
+ * visitor_stats plugin for Cotonti 0.9.26, PHP 8.4+
+ * Filename: visitor_stats.setup.php
+ *
+ * ReadMeMore:       https://github.com/webitproff/visitor-stats-crawler-cotonti/blob/main/README.md 
+ * Support:          https://abuyfile.com/ru/market/cotonti/plugs/visitor-stats-crawler-cotonti
+ *
+ * Date: June 23Th, 2026
+ * @package visitor_stats
+ * @version 1.0.27
+ * @author webitproff
+ * @copyright Copyright (c) webitproff 2026 | https://github.com/webitproff/visitor-stats-crawler-cotonti
+ * @license BSD
+ */
